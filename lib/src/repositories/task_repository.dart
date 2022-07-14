3283ca7d2dd52d0ca7878cbee9c29237b9e9b39a
Alexander Dischberg <noxasch@gmail.com>
2022-07-14 17:48:18 +0800

    implement task repository

---
+++ lib/src/repositories/task_repository.dart
@@ -0,0 +1,30 @@
+import 'package:drift/drift.dart';
+import 'package:drift_sqlite/src/db/app_database.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+part 'task_repository.g.dart';
+
+final tasksProvider = StreamProvider.autoDispose<List<Task>>((ref) {
+  return ref.watch(taskRepoProvider).watchAllTasks;
+});
+
+final taskRepoProvider = Provider((ref) => TaskRepository(AppDatabase()));
+
+@DriftAccessor(tables: [Tasks])
+class TaskRepository extends DatabaseAccessor<AppDatabase>
+    with _$TaskRepositoryMixin {
+  TaskRepository(AppDatabase db) : super(db);
+
+  Future<List<Task>> get getAllTasks => select(tasks).get();
+
+  Stream<List<Task>> get watchAllTasks => select(tasks).watch();
+
+  Future<Task> getById(int id) =>
+      (select(tasks)..where((t) => t.id.equals(id))).getSingle();
+
+  Future<int> addTask(TasksCompanion task) => into(tasks).insert(task);
+
+  Future<bool> updateTask(Task task) => update(tasks).replace(task);
+
+  Future<int> deleteTask(Task task) => delete(tasks).delete(task);
+}
---
+++ lib/src/repositories/task_repository.g.dart
@@ -0,0 +1,11 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'task_repository.dart';
+
+// **************************************************************************
+// DaoGenerator
+// **************************************************************************
+
+mixin _$TaskRepositoryMixin on DatabaseAccessor<AppDatabase> {
+  $TasksTable get tasks => attachedDatabase.tasks;
+}
